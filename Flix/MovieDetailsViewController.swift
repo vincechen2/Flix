613547c1581603e137191756a713e28af21c9ed9
Vincent Chen <vincent.chen.1@stonybrook.edu>
2021-09-18 11:25:57 -0400

    part 2

--- Flix/MovieDetailsViewController.swift
+++ Flix/MovieDetailsViewController.swift
@@ -4,13 +4,33 @@
 //
 //  Created by Vincent Chen on 9/17/21.
 //
-
+import AlamofireImage
 import UIKit
 
 class MovieDetailsViewController: UIViewController {
-
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    var movie: [String:Any]!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl =  URL(string: baseUrl + posterPath)
+        posterView.af_setImage(withURL: posterUrl!)
+
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl =  URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        backdropView.af_setImage(withURL: backdropUrl!)
+        
+        
+        synopsisLabel.text = movie["overview"] as! String
+        titleLabel.text = movie["title"] as! String
+        titleLabel.sizeToFit()
+        synopsisLabel.sizeToFit()
 
         // Do any additional setup after loading the view.
     }
--- Flix/MovieGridCell.swift
+++ Flix/MovieGridCell.swift
@@ -8,5 +8,6 @@
 import UIKit
 
 class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
     
 }
